d5163780725a931e335916842aa6a4b8ec8bb8e6
Prayag <165915472+ElementalcodeIn@users.noreply.github.com>
2025-06-27 16:04:28 +0530

    ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,20 @@ import GrowthApp from './pages/Home'
 import Setting from './pages/Setting'
 import Profile from './pages/Profile'
 import Meal from './pages/Meal'
-
 import GrowthTracker from './pages/GrowthTracker'
-
+import Dashboard from './pages/Dashboard' // <-- import Dashboard
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <Routes>
-      <Route path="/" element={<GrowthApp/>} />
-      <Route path="/setting" element={<Setting/>} />
-      <Route path="/profile" element={<Profile/>} />
-      <Route path="/meal" element={<Meal/>} />
-      <Route path="/growth" element={<GrowthTracker/>} />
-
+      <Route path="/" element={<Dashboard />} /> {/* Show Dashboard on home */}
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/setting" element={<Setting />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/meal" element={<Meal />} />
+      <Route path="/growth" element={<GrowthTracker />} />
     </Routes>
   )
 }
